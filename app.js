2c0222cea40ca21ea408e5f4d1f864d9b1084767
Koleaje Olayinka <koleajeolayinka@gmail.com>
2022-07-12 17:45:26 +0100

    json

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 8080;
-
-app.get('/', (req, res) => res.send('This is my first Cloud project'));
-
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
